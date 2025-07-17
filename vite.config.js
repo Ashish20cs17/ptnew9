133b6d616f549c9cb0036a937aea7503c570fd3c
Ashish20cs17 <89733738+Ashish20cs17@users.noreply.github.com>
2025-07-17 16:46:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
 // vite.config.js
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
   server: {
     host: '0.0.0.0',
-    allowedHosts: ['ptnew9.onrender.com'], // ✅ This allows the Render domain
-    // port: 3000, // Optional: Uncomment if needed
+    allowedHosts: 'all', // ✅ Allow all domains
   },
 });
